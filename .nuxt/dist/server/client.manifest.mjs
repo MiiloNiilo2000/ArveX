89f8157cf7dfe6d68804b2ddf7a54ae50c5a067a
Sander Talisoo <satali@taltech.ee>
2024-10-03 14:23:36 +0300

    Test222222

--- .nuxt/dist/server/client.manifest.mjs
+++ .nuxt/dist/server/client.manifest.mjs
@@ -1,18 +1,1 @@
-export default {
-  "@vite/client": {
-    "prefetch": true,
-    "isEntry": true,
-    "file": "@vite/client",
-    "css": [],
-    "module": true,
-    "resourceType": "script"
-  },
-  "C:/Users/37253/.vscode/itb2203_2024_arvex/node_modules/.pnpm/nuxt@3.13.2_@parcel+watcher@2.4.1_@types+node@22.7.4_ioredis@5.4.1_magicast@0.3.5_rollup@4.23_ezc75yl4ftuhtjdnf726wxgnsa/node_modules/nuxt/dist/app/entry.js": {
-    "resourceType": "script",
-    "module": true,
-    "prefetch": true,
-    "preload": true,
-    "isEntry": true,
-    "file": "C:/Users/37253/.vscode/itb2203_2024_arvex/node_modules/.pnpm/nuxt@3.13.2_@parcel+watcher@2.4.1_@types+node@22.7.4_ioredis@5.4.1_magicast@0.3.5_rollup@4.23_ezc75yl4ftuhtjdnf726wxgnsa/node_modules/nuxt/dist/app/entry.js"
-  }
-}
+export { default } from "file:///C:/Users/37253/.vscode/itb2203_2024_arvex/node_modules/.pnpm/@nuxt+vite-builder@3.13.2_@types+node@22.7.4_magicast@0.3.5_rollup@4.23.0_terser@5.34.1_vue@3.5.10/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
