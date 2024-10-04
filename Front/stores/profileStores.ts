9fc93ef2f301a08ad5141896ccfd844c3db525ca
Sander Talisoo <satali@taltech.ee>
2024-10-04 16:39:23 +0300

    Profiil kajastub pinias

---
+++ Front/stores/profileStores.ts
@@ -0,0 +1,30 @@
+import type { Profile } from "~/types/profile";
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
+
+export const useProfileStore = defineStore('profile', () => {
+  let i: number = 1;
+  
+  const profiles = ref<Profile[]>([
+    { username: 'Sviit Hõum', 
+        bio: 'Test, test', 
+        email: 'sviithoum@email.com', 
+        id: i++,
+        image: 'https://wallpapers.com/images/hd/funny-profile-picture-1l2l3tmmbobjqd53.jpg'
+     },
+  ]);
+
+  const addProfile = (profile: Profile) => {
+    profile.id = i++;
+    profiles.value.push(profile);
+  };
+
+  const deleteProfile = (id: number) => {
+    const index = profiles.value.findIndex((profile) => profile.id === id);
+    if (index !== -1) {
+      profiles.value.splice(index, 1);
+    }
+  };
+
+  return { profiles, addProfile, deleteProfile };
+});
---
+++ Front/types/profile.ts
@@ -0,0 +1,9 @@
+import type { UseLinkOptions } from "vue-router"
+
+export type Profile = {
+    id: number
+    username: string
+    bio: string
+    email: string
+    image: string
+}
