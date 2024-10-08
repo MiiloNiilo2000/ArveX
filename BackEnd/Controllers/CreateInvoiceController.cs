d95072df7f7dc248d2a3420dae9bd1586627b6dd
Steven Ernits <sterni@taltech.ee>
2024-10-08 19:53:35 +0300

    Lisatud mallile parempoole komponent

--- BackEnd/Controllers/CreateInvoiceController.cs
+++ BackEnd/Controllers/CreateInvoiceController.cs
@@ -37,7 +37,7 @@ namespace BackEnd.Controllers
                 container.Page(page =>
                 {
                     page.Size(PageSizes.A4);
-                    page.Margin(2, Unit.Centimetre);
+                    page.Margin(40);
                     page.PageColor(Colors.White);
                     page.DefaultTextStyle(x => x.FontSize(20));
 
@@ -64,8 +64,31 @@ namespace BackEnd.Controllers
                             x.Item().Text("12345, Tallinn").FontSize(15);
                             x.Item().Text("Eesti").FontSize(15);
                         });
-                        table.Cell().Row(1).Column(2).Text("B");
+                        table.Cell().Row(1).Column(2).Column(x =>
+                        {
+                            x.Item().Table(innerTable =>
+                            {
+                                innerTable.ColumnsDefinition(innerColumns =>
+                                {
+                                    innerColumns.RelativeColumn(); 
+                                    innerColumns.RelativeColumn(); 
+                                });
+                                innerTable.Cell().Row(1).Column(1).AlignLeft().Text("Arve number:").FontSize(15).Bold();
+                                innerTable.Cell().Row(1).Column(2).AlignRight().Text("12345").FontSize(15).Bold();
 
+                                innerTable.Cell().Row(2).Column(1).AlignLeft().Text("Kuupäev:").FontSize(12);
+                                innerTable.Cell().Row(2).Column(2).AlignRight().Text("12.07.2024").FontSize(12);
+
+                                innerTable.Cell().Row(3).Column(1).AlignLeft().Text("Tingimused:").FontSize(12);
+                                innerTable.Cell().Row(3).Column(2).AlignRight().Text("2 kuud").FontSize(12);
+
+                                innerTable.Cell().Row(4).Column(1).AlignLeft().Text("Maksetähtaeg:").FontSize(12);
+                                innerTable.Cell().Row(4).Column(2).AlignRight().Text("12.09.2024").FontSize(12);
+
+                                innerTable.Cell().Row(5).Column(1).AlignLeft().Text("Viivis:").FontSize(12);
+                                innerTable.Cell().Row(5).Column(2).AlignRight().Text("5% päevas").FontSize(12);
+                            });
+                        });
                     });
                         
                         //     x.Item()
