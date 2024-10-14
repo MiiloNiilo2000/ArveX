b03648ac90a0ed4fc371cf3d1e85c29a8616b55c
Markus Järavere <majara@taltech.ee>
2024-10-14 19:09:24 +0300

    Edit product component ja page lisatud. Editida veel ei saa

---
+++ Front/components/EditProduct.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="flex justify-center items-center pt-9">
+      <div class="w-96 p-6 flex flex-col h-99">
+        <h2 class="text-2xl font-bold mb-4 text-center">Muuda toode</h2>
+        <UForm
+          :validate="validate"
+          :state="state"
+          class="space-y-4"
+          @submit="onSubmit"
+          @error="onError"
+        >
+          <UFormGroup label="Toote nimi" name="name">
+            <UInput v-model="state.name" />
+          </UFormGroup>
+          <UFormGroup label="Kirjeldus" name="description">
+            <UInput v-model="state.description" />
+          </UFormGroup>
+          <UFormGroup label="Hind" name="price">
+            <UInput v-model="state.price" />
+          </UFormGroup>
+          <UButton type="submit"> Salvesta </UButton>
+        </UForm>
+      </div>
+    </div>
+  </template>
+
+<script setup lang="ts">
+import type { FormError, FormErrorEvent, FormSubmitEvent } from "#ui/types";
+import type { Product } from "~/types/product";
+import { reactive, onMounted } from 'vue';
+import { useProductStore } from '~/stores/productStores';
+import { useRoute } from 'vue-router';
+
+
+const{editProduct, getProductById} = useProductStore();
+
+const state = reactive<Product>({
+    id: 0,
+    name: '',
+    description: '',
+    price: 0,
+  });
+
+  // Get the current route to access the product ID
+const route = useRoute();
+const productId = Number(route.params.id); // Extract product ID from route params
+
+// Fetch product details when the component is mounted
+onMounted(async () => {
+  const product = await getProductById(productId); // Fetch product by ID
+  if (product) {
+    Object.assign(state, product); // Update the state with the product details
+  }
+});
+
+  const validate = (state: any): FormError[] => {
+    const errors = [];
+    if (!state.name) errors.push({ path: "name", message: "Required" });
+    if (!state.description) errors.push({ path: "description", message: "Required" });
+    if (!state.price) errors.push({ path: "price", message: "Required" });
+    return errors;
+  };
+
+  async function onSubmit(event: FormSubmitEvent<any>) {
+    console.log("Submitting product:", { ...state });
+    editProduct({ ...state });
+    await navigateTo("/products");
+  }
+
+  async function onError(event: FormErrorEvent) {
+    const element = document.getElementById(event.errors[0].id);
+    element?.focus();
+    element?.scrollIntoView({ behavior: "smooth", block: "center" });
+  }
+  </script>
