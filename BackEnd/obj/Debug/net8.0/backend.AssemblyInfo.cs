91cb475b73e81afea1960692c9eda8f096f64ad6
Markus Järavere <majara@taltech.ee>
2024-10-14 15:21:07 +0300

    ...

--- BackEnd/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ BackEnd/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,15 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42f5b17ae17c0ba2b66de1a24b7a35e8f88d60cd")]
-=======
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcfd7a272f9bfa9232aaa51c8f8db9e9ac72231b")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+349fc72b0ba34db0694b486557b878ced763f1ba")]
->>>>>>> cbd78c654972f30f7517d36ac4a47a993d387b90
->>>>>>> c98b8d6d141e9f78cd5ee41b61fc1714e5c61546
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3b2ca48be3ebe54d4472201454cb91a801240a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
