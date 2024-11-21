7f33120e0de094416673e47d15242216ac00bd0e
Sander Talisoo <satali@taltech.ee>
2024-11-21 14:35:42 +0200

    Kui kasutaja on sisse logitud muutub sisselogimise nupp

--- Front/components/TheMenu.vue
+++ Front/components/TheMenu.vue
@@ -1,49 +1,60 @@
-<<template>
+<template>
   <div class="flex justify-start items-center border-b border-gray-200 dark:border-gray-800">
     <UHorizontalNavigation
       :links="links"
       class="flex"
     />
     <NuxtLink
+      v-if="!isLoggedIn"
       to="/login"
       class="bg-blue-500 text-white px-8 py-2 rounded-lg shadow-md font-semibold hover:bg-blue-600"
     >
       Sisselogimine
     </NuxtLink>
+    <NuxtLink
+      v-else
+      to="/profiles"
+      class="bg-blue-500 text-white px-8 py-2 rounded-lg shadow-md font-semibold hover:bg-blue-600"
+    >
+      Profiil
+    </NuxtLink>
   </div>
 </template>
-  
-  <script setup lang="ts">
-  import { useRoute } from 'vue-router';
 
-  const route = useRoute();
-  
-  const links = [
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+import { ref, computed } from 'vue';
+
+const isLoggedIn = computed(() => {
+  return localStorage.getItem('token') !== null;
+});
+
+const route = useRoute();
+
+const links = [
   {
-      label: "Avaleht",
-      to: "/",
-    },
-    {
-      label: "Profiil",
-      to: "/profiles",
-    },
-    {
-      label: "Tooted",
-      to: "/products",
-    },
-    {
-      label: "Loo Arve",
-      to: "/create-invoice",
-    },
-    {
-      label: "Ajalugu",
-      to: "/view-history",
-    },
-    {
-      label: "Test",
-      to: "/test-home",
-    }
-    
-  ];
-  
-  </script>
+    label: "Avaleht",
+    to: "/",
+  },
+  {
+    label: "Profiil",
+    to: "/profiles",
+  },
+  {
+    label: "Tooted",
+    to: "/products",
+  },
+  {
+    label: "Loo Arve",
+    to: "/create-invoice",
+  },
+  {
+    label: "Ajalugu",
+    to: "/view-history",
+  },
+  {
+    label: "Test",
+    to: "/test-home",
+  }
+];
+</script>
