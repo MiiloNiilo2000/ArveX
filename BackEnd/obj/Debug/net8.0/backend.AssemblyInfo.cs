0d028297b44c82c6e4dadfa5b57966a248570189
Steven Ernits <sterni@taltech.ee>
2024-11-22 14:37:51 +0200

    Pull

--- BackEnd/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ BackEnd/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ee97d3308da056ce30c82095dab93e892152dfc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4406e696ed384de2c5c858a03eb94d9e82a0a898")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
